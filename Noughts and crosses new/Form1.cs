bc313ca8112621a9b0496ecdd123cde5f2ec515c
Luke Barkess <luke.barkess@icloud.com>
2022-11-11 10:19:10 +0000

    asd

--- Noughts and crosses new/Form1.cs
+++ Noughts and crosses new/Form1.cs
@@ -14,14 +14,16 @@ namespace Noughts_and_crosses_new
     {
         string cross = "X";
         string nought = "O";
-        //string userMark = cross;
-        //string computerMark = nought;
+        string currentSymbol;
+        bool computerPlayerEnabled = false;
+        Random random = new Random();
 
         Button[,] grid;
 
         public Form1()
         {
             InitializeComponent();
+            currentSymbol = cross;
 
             grid = new Button[,] { { btn00, btn10, btn20},
                                     { btn01, btn11, btn21 },
@@ -41,16 +43,30 @@ namespace Noughts_and_crosses_new
             Button btn = (Button)sender;
             int x_coord = int.Parse(btn.Name[3].ToString());
             int y_coord = int.Parse(btn.Name[4].ToString());
-            MessageBox.Show(x_coord.ToString() + y_coord.ToString());
 
             if (btn.Text == string.Empty)
             {
+                //btn.Text = currentSymbol;
                 btn.Text = cross;
+
                 if (WinCheck(cross))
                 {
-                    MessageBox.Show("Someone" + " has won!");
-                };
-                // switch turn
+                    MessageBox.Show(currentSymbol + " has won!");
+                    ClearBoard();
+                    currentSymbol = cross;
+                    return;
+                }
+
+                SwitchSymbol();
+                PlayComputerMove();
+
+                if (WinCheck(nought))
+                {
+                    MessageBox.Show("The computer has won!");
+                    ClearBoard();
+                    currentSymbol = cross;
+                    return;
+                }
             }
         }
 
@@ -86,7 +102,40 @@ namespace Noughts_and_crosses_new
                 return true;
             }
 
+            foreach (Button button in grid)
+            {
+                if (button.Text == string.Empty)
+                {
+
+                }
+            }
+
             return false;
+        }
+
+        private void ClearBoard()
+        {
+            foreach (Button button in grid)
+            {
+                button.ResetText();
+            }
+        }
+
+        private void SwitchSymbol()
+        {
+            currentSymbol = currentSymbol == cross ? nought : cross;
+        }
+
+        private void PlayComputerMove()
+        {
+            Button possibleMove = grid[random.Next(0, grid.GetLength(0)), random.Next(0, grid.GetLength(1))];
+
+            while (possibleMove.Text != string.Empty)
+            {
+                possibleMove = grid[random.Next(0, grid.GetLength(0)), random.Next(0, grid.GetLength(1))];
+            }
+
+            possibleMove.Text = nought;
         }
     }
 }
